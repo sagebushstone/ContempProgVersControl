b16639a81f9701548c9514b082ffd48b47e93d6d
Sage Bushstone <sage.bushstone@gmail.com>
2023-10-27 12:33:24 -0400

    created project

---
+++ sage_bushstone_versioncontrol/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace sage_bushstone_versioncontrol.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
