005416a1b098bf59a4e6ad453ed2cf48b1e998e3
Sage Bushstone <sage.bushstone@gmail.com>
2023-10-27 12:35:01 -0400

    Created controller and added JSON method

---
+++ sage_bushstone_versioncontrol/Controllers/NameController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace sage_bushstone_versioncontrol.Controllers
+{
+    public class NameController : Controller
+    {
+        public string JsonName()
+        {
+            return "{[\"Sage Bushstone\"]}";
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
